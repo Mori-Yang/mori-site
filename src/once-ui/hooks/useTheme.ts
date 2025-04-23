137a0d4127b54d96897a70fe86ca2f24efc8c407
Moir <ys0514@yeah.net>
2025-04-23 14:14:41 +0800

    fix: useTheme check window

--- src/once-ui/hooks/useTheme.ts
+++ src/once-ui/hooks/useTheme.ts
@@ -1,9 +1,14 @@
 "use client"
 import { useCallback, useEffect, useState } from "react"
+function getSysTheme() {
+    if (typeof window !== 'undefined') {
+        return window.matchMedia('(prefers-color-scheme: dark)').matches ? "dark" : "light"
+    }
+    return "light"
+}
 
 export const useTheme = (): [string, () => void] => {
-    const sysTheme = matchMedia('(prefers-color-scheme: dark)').matches ? "dark" : "light"
-    const [theme, setTheme] = useState(sysTheme)
+    const [theme, setTheme] = useState(getSysTheme())
 
     const toggleTheme = useCallback(() => {
         if (theme === 'light') {
@@ -16,6 +21,14 @@ export const useTheme = (): [string, () => void] => {
     useEffect(() => {
         const root = document.documentElement;
         root.setAttribute('data-theme', theme)
+
+        const mediaQuery = window.matchMedia('(prefers-color-scheme: dark)')
+        const handler = (e: MediaQueryListEvent) => {
+            setTheme(e.matches ? "dark" : "light")
+        }
+        mediaQuery.addEventListener('change', handler)
+
+        return () => mediaQuery.removeEventListener('change', handler)
     }, [theme])
 
     return [theme, toggleTheme]
